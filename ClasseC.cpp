6718c4efe3b0e824b4193622a1032cea378c1761
Guilherm Melo <devilnero2011@gmail.com>
2019-04-14 19:50:19 -0300

    Adicionando na classeC o método MC3()

---
+++ ClasseC.cpp
@@ -0,0 +1,21 @@
+class ClasseC {
+  private:
+    string C1;
+    int C2;
+  public:
+    string getC1(){
+      return C1;
+    }
+    int getC2(){
+      return C2; 
+    }
+    void MC1(){
+      cout << "MC1" << endl;
+    }
+    void MC2(){
+      cout << "MC2" << endl;
+    }
+    void MC3(){
+        cout << "MC3" << endl;
+    }
+};
