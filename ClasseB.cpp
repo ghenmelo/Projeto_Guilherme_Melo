f43ab955c0f68869788e501c3d7965a12001a07c
Guilherme Melo <33105000+gjluffy@users.noreply.github.com>
2019-04-14 18:16:38 -0300

    Criando classe B

---
+++ ClasseB.cpp
@@ -0,0 +1,18 @@
+class ClasseB {
+  private:
+    int B1;
+    float B2;
+  public:
+    int getB1(){
+      return B1;
+    }
+    float getB2(){
+      return B2; 
+    }
+    void MB1(){
+      cout << "MB1" << endl;
+    }
+    void MB2(){
+      cout << "MB1" << endl;
+    }
+};
