878dfe0334a19f230b9056ca4d5b8e8e6d7396c8
Guilherme Melo <33105000+gjluffy@users.noreply.github.com>
2019-04-14 18:15:47 -0300

    Criando classe A

---
+++ ClasseA.cpp
@@ -0,0 +1,18 @@
+class ClasseA {
+  private:
+    int A1;
+    float A2;
+  public:
+    int getA1(){
+      return A1;
+    }
+    float getA2(){
+      return A2; 
+    }
+    void MA1(){
+      cout << "MA1" << endl;
+    }
+    void MA2(){
+      cout << "MA1" << endl;
+    }
+};
