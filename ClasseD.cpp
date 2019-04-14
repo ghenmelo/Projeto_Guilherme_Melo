4663fd1ef875595257f96aa10ace6c017788cf88
Guilherm Melo <devilnero2011@gmail.com>
2019-04-14 19:50:54 -0300

    Criando nova classe

---
+++ ClasseD.cpp
@@ -0,0 +1,15 @@
+class ClasseD{
+    private:
+        int D1;
+        int D2;
+    public:
+        void MD1(){
+            cout << "MD1" << endl;
+        }
+        void MD2(){
+            cout << "MD2" << endl;
+        }
+
+}
+
+
